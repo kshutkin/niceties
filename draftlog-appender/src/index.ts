677c2795f6f300fe9e720dd5ea9ac0effb881112
Konstantin Shutkin <kostya80@gmail.com>
2021-12-28 21:06:26 +0400

    feat: add default message filter for draftlog appender

--- draftlog-appender/src/index.ts
+++ draftlog-appender/src/index.ts
@@ -1,12 +1,12 @@
-import { appender } from '@niceties/logger/core';
+import { appender, filterMessages } from '@niceties/logger/core';
 import { terminalSupportsUnicode, createFormatter } from '@niceties/logger/format-utils';
 import { colors, unicodePrefixes, asciiPrefixes } from '@niceties/logger/default-formatting';
 import { createDraftlogAppender } from './core';
 import { dots, line } from './spinners';
+import { Action, LogLevel, LogMessage } from '@niceties/logger';
 
 const supportsUnicode = terminalSupportsUnicode();
 const spinner = supportsUnicode ? dots : line;
 const formatter = createFormatter(colors, supportsUnicode ? unicodePrefixes : asciiPrefixes);
 
-appender(createDraftlogAppender(spinner, formatter, false, 2));
-
+appender(filterMessages((message: LogMessage) => message.loglevel !== LogLevel.verbose || message.action !== Action.log, createDraftlogAppender(spinner, formatter, false, 2)));
