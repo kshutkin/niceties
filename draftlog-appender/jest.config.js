b909a8e81b8ce47341bd30787688fd4caae17f8e
Konstantin Shutkin <kostya80@gmail.com>
2021-12-29 16:01:20 +0400

    config: fixed descriptions and linting warnings

--- draftlog-appender/jest.config.js
+++ draftlog-appender/jest.config.js
@@ -1,19 +1,19 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts'],
-  testMatch: ['<rootDir>/**/tests/**/*.spec.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    extensionsToTreatAsEsm: ['.ts'],
+    testMatch: ['<rootDir>/**/tests/**/*.spec.ts'],
+    globals: {
+        'ts-jest': {
+            useESM: true,
+        },
     },
-  },
-  testPathIgnorePatterns: ['/node_modules/'],
-  coverageDirectory: './coverage',
-  coveragePathIgnorePatterns: ['node_modules', 'src/tests'],
-  moduleNameMapper: {
-    "@niceties/(.*)/(.*)": "<rootDir>/../$1/src/$2.ts",
-    "@niceties/(.*)$": "<rootDir>/../$1/src",
-  },
+    testPathIgnorePatterns: ['/node_modules/'],
+    coverageDirectory: './coverage',
+    coveragePathIgnorePatterns: ['node_modules', 'src/tests'],
+    moduleNameMapper: {
+        '@niceties/(.*)/(.*)': '<rootDir>/../$1/src/$2.ts',
+        '@niceties/(.*)$': '<rootDir>/../$1/src',
+    },
 };
