dae07472d03a3e6661354007e7493f4ab8aff25a
Konstantin Shutkin <kostya80@gmail.com>
2022-11-22 20:48:21 +0100

    chore: fix html, added console.log in check-result

--- examples/check-result.mjs
+++ examples/check-result.mjs
@@ -4,4 +4,6 @@ const result = fs.readFileSync('./result.log');
 
 const isFine = result.toString() === 'info message\nwarning message\nerror message\n';
 
+console.log(isFine ? 'fine!' : 'error');
+
 process.exit(isFine ? 0 : -1);
