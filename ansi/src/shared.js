269121d2296e119621594f00aa6c2eefade095d4
Konstantin Shutkin <kostya80@gmail.com>
2026-03-15 18:27:41 +0100

    deoptimize

--- ansi/src/shared.js
+++ ansi/src/shared.js
@@ -3,25 +3,6 @@
  */
 
 /**
- * @param {string} string
- * @param {string} close
- * @param {string} replace
- * @param {number} index
- * @returns {string}
- */
-const replaceClose = (string, close, replace, index) => {
-    const closeLength = close.length;
-    let result = string.substring(0, index) + replace;
-    let cursor = index + closeLength;
-    // biome-ignore lint/suspicious/noAssignInExpressions: optimization
-    while (~(index = string.indexOf(close, cursor))) {
-        result += string.substring(cursor, index) + replace;
-        cursor = index + closeLength;
-    }
-    return result + string.substring(cursor);
-};
-
-/**
  * @param {string} open
  * @param {string} close
  * @param {string} [replace=open]
@@ -32,7 +13,6 @@ export const formatter = (open, close, replace = open) => {
     return input => {
         // biome-ignore lint/style/useTemplate: optimization
         const string = '' + input;
-        const index = string.indexOf(close, skip);
-        return ~index ? open + replaceClose(string, close, replace, index) + close : open + string + close;
+        return ~string.indexOf(close, skip) ? open + string.replaceAll(close, replace) + close : open + string + close;
     };
 };
